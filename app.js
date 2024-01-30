14a209e63e633c54bcc7d3074e7196790e4d1da4
Felipe-Cordeiro-98 <142260275+Felipe-Cordeiro-98@users.noreply.github.com>
2024-01-29 21:52:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let numeroSecreto = gerarNumeroAleatorio();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2});
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1 });
 }
 
 exibirTextoNaTela("h1", "Jogo do número secreto");
@@ -14,9 +14,13 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela("h1", "Parabéns, você acertou!");
         exibirTextoNaTela("p", "O número secreto é: " + numeroSecreto);
-    };
+    } else if (chute > numeroSecreto) {
+        exibirTextoNaTela("p", `O número e maior que ${chute}`);
+    } else {
+        exibirTextoNaTela("p", `O número e menor que ${chute}`);
+    }
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1)
+    return parseInt(Math.random() * 10 + 1);
 }
